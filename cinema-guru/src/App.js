fb7092ce4e42e66ee1f141914e9c91e5f86f8b58
Rémi Marçais <remi.marcais87@gmail.com>
2023-06-30 18:27:47 +0200

    Initialize the main App component

--- cinema-guru/src/App.js
+++ cinema-guru/src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
 
-function App() {
+export default function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [userUsername, setUserUsername] = useState("");
+
+  useEffect(() => {
+    const accessToken = localStorage.getItem('accessToken');
+    const headers = {authorization: `Bearer ${accessToken}`}
+
+    axios.post('http://localhost:8000/api/auth/', {}, headers)
+    .then((res) => {
+      if (res.status === 200) {
+        setIsLoggedIn(true);
+        setUserUsername(res.data.username);
+      }
+    });
+  });
+//App must return depending on the isLoggedIn state :
+// - true: The Dashboard component (Will be built in later tasks)
+// - false: The Authentication component (Will be built in later tasks)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {isLoggedIn && <p>wahoouuuuu {userUsername}</p>}
+      {!isLoggedIn && <p>Pas connecté</p>}
     </div>
   );
 }
-
-export default App;
